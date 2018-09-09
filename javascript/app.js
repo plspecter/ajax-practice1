e966645c1ae9feb807dca3e603458a4d3d314c8d
Perri Specter <plspecter@gmail.com>
2018-09-09 12:17:20 -0400

    ajax-practice1

---
+++ javascript/app.js
@@ -0,0 +1,37 @@
+$(document).ready(); {
+    console.log("we work")
+
+    $("press").on("click", function() {
+        console.log("clicked")
+
+
+
+        var person = $(this).attr("data-person")
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=LADom26qErILrvxvUKoDrk3kmFT6jhiO";
+
+        //Ajax time
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>");
+
+                var gifImage = $("<img>");
+
+                gifImage.attr("src", results[i].images.fixed_height.url);
+
+                $("$gifs-appear").prepend(gifDiv);
+
+            }
+
+        });
+
+
+    });
+
+};
